c9c84906eb42619ca0ba48133a5edf9c14bd2bd6
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-02-10 09:50:12 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,147 @@
+<h1 align="center">SmartDlBot Telegram Bot 🌌</h1>
+
+<p align="center">
+  <em>SmartDlBot: The ultimate toolkit on Telegram, offering Facebook,YouTube,Pinterest,Spotify Downloader. Simplify your tasks with ease!</em>
+</p>
+
+<p align="center">
+  <a href="https://github.com/abirxdhack/SmartDlBot/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/abirxdhack/SmartDlBot"></a>
+  <a href="https://github.com/abirxdhack/SmartDlBot/network/members"><img alt="GitHub forks" src="https://img.shields.io/github/forks/abirxdhack/SmartDlBot"></a>
+  <a href="https://github.com/abirxdhack/SmartDlBot/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/abirxdhack/SmartDlBot"></a>
+  <a href="https://github.com/abirxdhack/SmartDlBot/blob/main/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/abirxdhack/SmartDlBot"></a>
+  <a href="https://github.com/abirxdhack/SmartDlBot/pulls"><img alt="GitHub pull requests" src="https://img.shields.io/github/issues-pr/abirxdhack/SmartDlBot"></a>
+  <img alt="PyPI - Python Version" src="https://img.shields.io/pypi/pyversions/pyrogram">
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/abirxdhack/SmartDlBot">
+</p>
+
+<hr>
+
+## 🌟 Features
+
+- **Facebook Video Downloader**: Download videos from Facebook.
+- **YouTube Video Downloader**: Download videos from YouTube.
+- **Pinterest Video Downloader**: Download videos from Pinterest.
+- **Spotify Track Downloader**: Download tracks from Spotify.
+
+## Requirements
+
+Before you begin, ensure you have met the following requirements:
+
+- 🐍 Python 3.9 or higher.
+- 📦 `pyrogram` , `yt-dlp` , `spotipy` , `requests` , `pillow` , `asyncio` , `aiofiles` , `aiohttp`  library.
+- 🤖 A Telegram bot token (you can get one from [@BotFather](https://t.me/BotFather) on Telegram).
+- 🔑 API ID and Hash: You can get these by creating an application on [my.telegram.org](https://my.telegram.org).
+- 🎸 SPOTIFY_CLIENT_ID from [Spotify Developer Dashboard](https://developer.spotify.com/dashboard)
+- 🎸 SPOTIFY_CLIENT_SECRET from [Spotify Developer Dashboard](https://developer.spotify.com/dashboard)
+
+## Installation
+
+To install `pyrogram` , `yt-dlp` , `spotipy` , `requests` , `pillow` , `asyncio` , `aiofiles` , `aiohttp`  run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+**Note: If you previously installed `pyrofork`, uninstall it before installing `pyrogram`.**
+
+## Handling YouTube Download Errors with Cookies
+
+To avoid errors related to YouTube sign-in requirements, using a cookie file is effective. Here's how to set it up:
+
+### Steps to Export and Use Cookies:
+
+1. **Create a Dedicated Chrome Profile:**
+   - It's recommended to create a new Chrome profile for managing your bot's cookies.
+
+2. **Install a Cookie Management Extension:**
+   - Use "Cookie Editor" or similar extensions to manage your cookies.
+
+3. **Export Cookies from YouTube:**
+   - Log into YouTube in your new browser profile and export cookies in Netscape format via the cookie extension.
+
+4. **Save the Cookies File:**
+   - Update your `cookies.txt` in the `TelecastBot/ytcookies` directory of your project.
+
+### Managing Cookies:
+
+- **Cookie Expiry:**
+  - Refresh your cookies by exporting new ones if you encounter download issues.
+
+- **Cookie Depletion:**
+  - Avoid frequent bot restarts and excessive YouTube requests to prevent early cookie expiry.
+
+This setup should help manage YouTube content access efficiently without encountering sign-in or bot protection errors.
+
+## Configuration
+
+1. Open the `config.py` file in your favorite text editor.
+2. Replace the placeholders for `API_ID`, `API_HASH`,  `BOT_TOKEN`, `SPOTIFY_CLIENT_ID`, and `SPOTIFY_CLIENT_SECRET` with your actual values:
+   - **`API_ID`**: Your API ID from [my.telegram.org](https://my.telegram.org).
+   - **`API_HASH`**: Your API Hash from [my.telegram.org](https://my.telegram.org).
+   - **`BOT_TOKEN`**: The token you obtained from [@BotFather](https://t.me/BotFather).
+   - **`SPOTIFY_CLIENT_ID`**: SPOTIFY_CLIENT_ID from [Spotify Developer Dashboard](https://developer.spotify.com/dashboard)
+   - **`SPOTIFY_CLIENT_SECRET`**: SPOTIFY_CLIENT_SECRET from [Spotify Developer Dashboard](https://developer.spotify.com/dashboard)
+
+## Deploy the Bot
+
+```sh
+git clone https://github.com/abirxdhack/SmartDlBot
+cd SmartDlBot
+python main.py
+```
+
+## Usage 🛠️
+
+The bot supports the following commands:
+
+Download videos and tracks from popular platforms using these commands:
+
+➢ **`/fb [Video URL]`** - Download a Facebook video.
+   - Example: **/fb https://www.facebook.com/share/v/18VH1yNXoq/** **`Downloads the specified Facebook video`**
+   - Note: Private Facebook videos cannot be downloaded.
+
+➢ **`/pin [Video URL]`** - Download a Pinterest video.
+   - Example: **/pin https://pin.it/6GoDMRwmE** **`Downloads the specified Pinterest video`**
+   - Note: 18+ Instagram Reels cannot be downloaded.
+
+➢ **`/sp [Track URL]`** - Download a Spotify track.
+   - Example: **/sp https://open.spotify.com/track/7ouBSPZKQpm7zQz2leJXta** **`Downloads the specified Spotify track**`
+
+➢ **`/yt [Video URL]`** - Download a YouTube video.
+   - Example: **/yt https://youtu.be/In8bfGnXavw** **`Downloads the specified YouTube video`**
+
+➢ **`/song [Video URL]`** - Download a YouTube video as an MP3 file.
+   - Example: **/song https://youtu.be/In8bfGnXavw** **`Converts and downloads the video as MP3`**
+
+**Or You Can Use Below Commands **
+
+➢ **`.fb [Video URL]`** - Download a Facebook video.
+   - Example: **.fb https://www.facebook.com/share/v/18VH1yNXoq/** **`Downloads the specified Facebook video`**
+   - Note: Private Facebook videos cannot be downloaded.
+
+➢ **`.pin [Video URL]`** - Download a Pinterest video.
+   - Example: **.pin https://pin.it/6GoDMRwmE** **`Downloads the specified Pinterest video`**
+   - Note: 18+ Instagram Reels cannot be downloaded.
+
+➢ **`.sp [Track URL]`** - Download a Spotify track.
+   - Example: **.sp https://open.spotify.com/track/7ouBSPZKQpm7zQz2leJXta** **`Downloads the specified Spotify track`**
+
+➢ **`.yt [Video URL]`** - Download a YouTube video.
+   - Example: **.yt https://youtu.be/In8bfGnXavw** **`Downloads the specified YouTube video`**
+
+➢ **`.song [Video URL]`** - Download a YouTube video as an MP3 file.
+   - Example: **.song https://youtu.be/In8bfGnXavw** **`Converts and downloads the video as MP3`**
+
+##NOTE
+**Provide a valid public URL for each platform to download successfully**
+For inquiries or feedback, please feel free to reach out via Telegram.
+
+## Ethical Notice 🔔
+> **Ethics Reminder:** Simply modifying a few lines of code does not constitute original authorship. When forking a project, always fork responsibly and give proper credit to the original creators.
+
+# Project Contributors
+
+## Author 📝
+
+- Name: Abir Arafat Chawdhury
+- Telegram: [@abirxdhackz](https://t.me/abirxdhackz)
---
+++ main.py
@@ -0,0 +1,144 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import asyncio
+from pyrogram.enums import ParseMode
+import os
+from config import API_ID, API_HASH, BOT_TOKEN
+
+# Import the handlers
+from youtube.youtube import setup_downloader_handler
+from pinterest.pinterest import setup_pinterest_handler
+from facebook.facebook import setup_dl_handlers
+from spotify.spotify import setup_spotify_handler
+
+# Initialize the bot client
+app = Client(
+    "app_session",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+# Setup handlers
+setup_downloader_handler(app)
+setup_pinterest_handler(app)
+setup_dl_handlers(app)
+setup_spotify_handler(app)
+
+@app.on_message(filters.command(["start"], prefixes=["/", "."]) & filters.private)
+async def send_start_message(client, message):
+    chat_id = message.chat.id
+    full_name = f"{message.from_user.first_name} {message.from_user.last_name}" if message.from_user.last_name else message.from_user.first_name
+
+    # Animation messages
+    animation_message = await message.reply_text("<b>Starting Smart Tool ⚙️...</b>", parse_mode=ParseMode.HTML)
+    await asyncio.sleep(0.4)  # Use asyncio.sleep instead of sleep
+    await animation_message.edit_text("<b>Generating Session Keys Please Wait...</b>", parse_mode=ParseMode.HTML)
+    await asyncio.sleep(0.4)  # Use asyncio.sleep instead of sleep
+    await animation_message.delete()
+
+    # Main welcome message
+    start_message = (
+        f"<b>Hi {full_name}! Welcome To This Bot...</b>\n"
+        "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
+        "<b><a href='tg://user?id=7303810912'>Smart Tool ⚙️</a></b>: The ultimate toolkit on Telegram, offering Facebook,YouTube,Pinterest,Spotify Downloader. Simplify your tasks with ease!\n"
+        "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
+        "<b>Don't Forget To <a href='https://t.me/ModVipRM'>Join Here</a> For Updates!</b>"
+    )
+
+    await message.reply_text(
+        start_message,
+        parse_mode=ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("⚙️ Help", callback_data="help_menu"),
+             InlineKeyboardButton("➕ Add Me", url="https://t.me/ItsSmartToolBot?startgroup=new&admin=post_messages+delete_messages+edit_messages+pin_messages+change_info+invite_users+promote_members")],
+            [InlineKeyboardButton("🔄 Updates", url="https://t.me/ModVipRM"),
+             InlineKeyboardButton("ℹ️ About Me", callback_data="about_me")]
+        ]),
+        disable_web_page_preview=True,
+    )
+
+@app.on_callback_query(filters.regex("help_menu"))
+async def help_menu_callback(client: Client, callback_query: CallbackQuery):
+    help_message = (
+        "<b>🎥 Social Media and Music Downloader</b>\n"
+        "━━━━━━━━━━━━━━━━━━━━━━\n"
+        "<b>USAGE:</b>\n"
+        "Download videos and tracks from popular platforms using these commands:\n\n"
+        "➢ <b>/fb [Video URL]</b> - Download a Facebook video.\n"
+        "   - Example: <code>/fb https://www.facebook.com/share/v/18VH1yNXoq/</code> (Downloads the specified Facebook video)\n"
+        "   - Note: Private Facebook videos cannot be downloaded.\n\n"
+        "➢ <b>/pin [Video URL]</b> - Download a Pinterest video.\n"
+        "   - Example: <code>/pin https://pin.it/6GoDMRwmE</code> (Downloads the specified Pinterest video)\n\n"
+        "   - Note: 18+ Instagram Reels cannot be downloaded.\n\n"
+        "➢ <b>/sp [Track URL]</b> - Download a Spotify track.\n"
+        "   - Example: <code>/sp https://open.spotify.com/track/7ouBSPZKQpm7zQz2leJXta</code> (Downloads the specified Spotify track)\n\n"
+        "➢ <b>/yt [Video URL]</b> - Download a YouTube video.\n"
+        "   - Example: <code>/yt https://youtu.be/In8bfGnXavw</code> (Downloads the specified YouTube video)\n\n"
+        "➢ <b>/song [Video URL]</b> - Download a YouTube video as an MP3 file.\n"
+        "   - Example: <code>/song https://youtu.be/In8bfGnXavw</code> (Converts and downloads the video as MP3)\n\n"
+        ">NOTE:\n"
+        "1️⃣ Provide a valid public URL for each platform to download successfully.\n\n"
+        ">🔔 For Bot Update News: <a href='https://t.me/ModVipRM'>Join Now</a>"
+    )
+
+    await callback_query.message.edit_text(
+        help_message,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("🔙 Back", callback_data="start_menu")]
+        ])
+    )
+
+@app.on_callback_query(filters.regex("about_me"))
+async def about_me_callback(client: Client, callback_query: CallbackQuery):
+    about_message = (
+        "<b>Name:</b> Smart Tool ⚙️\n"
+        "<b>Version:</b> 3.0 (Beta Testing) 🛠\n\n"
+        "<b>Development Team:</b>\n"
+        "- <b>Creator:</b> <a href='https://t.me/abirxdhackz'>⏤͟͞〲ᗩᗷiᖇ 𓊈乂ᗪ𓊉 👨‍💻</a>\n"
+        "<b>Technical Stack:</b>\n"
+        "- <b>Language:</b> Python 🐍\n"
+        "- <b>Libraries:</b> Aiogram, Pyrogram And Telethon 📚\n"
+        "- <b>Database:</b> MongoDB Database 🗄\n"
+        "- <b>Hosting:</b> Hostinger VPS 🌐\n\n"
+        "<b>About:</b> Smart Tool ⚙️ The ultimate toolkit on Telegram, offering Facebook,YouTube,Pinterest,Spotify Downloader. Simplify your tasks with ease!\n\n"
+        ">🔔 For Bot Update News: <a href='https://t.me/ModVipRM'>Join Now</a>"
+    )
+
+    await callback_query.message.edit_text(
+        about_message,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("🔙 Back", callback_data="start_menu")]
+        ])
+    )
+
+@app.on_callback_query(filters.regex("start_menu"))
+async def start_menu_callback(client: Client, callback_query: CallbackQuery):
+    full_name = f"{callback_query.from_user.first_name} {callback_query.from_user.last_name}" if callback_query.from_user.last_name else callback_query.from_user.first_name
+
+    start_message = (
+        f"<b>Hi {full_name}! Welcome To This Bot...</b>\n"
+        "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
+        "<b><a href='tg://user?id=7303810912'>Smart Tool ⚙️</a></b>: The ultimate toolkit on Telegram, offering Facebook,YouTube,Pinterest,Spotify Downloader. Simplify your tasks with ease!\n"
+        "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
+        "<b>Don't Forget To <a href='https://t.me/ModVipRM'>Join Here</a> For Updates!</b>"
+    )
+
+    await callback_query.message.edit_text(
+        start_message,
+        parse_mode=ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("⚙️ Help", callback_data="help_menu"),
+             InlineKeyboardButton("➕ Add Me", url="https://t.me/ItsSmartToolBot?startgroup=new&admin=post_messages+delete_messages+edit_messages+pin_messages+change_info+invite_users+promote_members")],
+            [InlineKeyboardButton("🔄 Updates", url="https://t.me/ModVipRM"),
+             InlineKeyboardButton("ℹ️ About Me", callback_data="about_me")]
+        ]),
+        disable_web_page_preview=True,
+    )
+
+print("Bot is running...")
+app.run()
