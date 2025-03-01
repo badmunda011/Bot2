271b7aa53433c49c00bc195d1d247ab1b5e72bb3
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-03-01 10:32:49 +0600

    Env add

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-# Replace these with your actual API details
-API_ID = "28xx710"  # Replace with your API ID Get From my.telegram.org
-API_HASH = "7fc5bxxxxxcsfb5eca3"  # Replace with your API Hash Get From my.telegram.org
-BOT_TOKEN = "7824988xxccvipu88wmA9u1tOtq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
-# Spotify Configuration Get From https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = '5941bb8af55d4345rf97f616e325'
-SPOTIFY_CLIENT_SECRET = '408frrwhhderroa9eff8'  # Replace this with your actual Spotify client secret
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(',')))
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+MONGO_URL = os.getenv("MONGO_URL")
