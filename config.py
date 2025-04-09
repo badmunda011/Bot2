d0f7d9bccc40ce489fe671a54dd0ab5a4ca522ed
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-04-09 22:12:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ if ADMIN_IDS:
 else:
     ADMIN_IDS = [443809517]  # Default to an empty list if ADMIN_IDS is not set
 
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_CLIENT_ID = "5941bb8af55d41c5297f616e325"
+SPOTIFY_CLIENT_SECRET = "408f04b237aba1b8bfc5da9eff8"
 MONGO_URL = os.getenv("MONGO_URL")
 COMMAND_PREFIX = ["!", "/"]  # Example of common command prefixes
